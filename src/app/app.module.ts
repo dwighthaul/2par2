5bb02731cf0c63ae71196d4ed268a807606ed88a
hubertPaul <paulohubert@hotmail.fr>
2022-08-19 23:14:21 +0200

    Config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SudokuMapComponent } from './sudoku-map/sudoku-map.component';
+import { DeuxParDeuxComponent } from './deux-par-deux/deux-par-deux.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SudokuMapComponent,
+    DeuxParDeuxComponent
   ],
   imports: [
     BrowserModule
