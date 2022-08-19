025a358346379f1c0255d92c893d889ba555bcdf
hubertPaul <paulohubert@hotmail.fr>
2022-08-19 23:23:12 +0200

    Supression import inutiles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { SudokuMapComponent } from './sudoku-map/sudoku-map.component';
 import { DeuxParDeuxComponent } from './deux-par-deux/deux-par-deux.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SudokuMapComponent,
     DeuxParDeuxComponent
   ],
   imports: [
